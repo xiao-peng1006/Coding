5e054f3b528cedba61f47baecec9a68f85a5f8ea
Xiao Peng <xiao.peng1006@gmail.com>
2021-11-05 23:48:08 -0700

    double stack

--- 21. Merge Two Sorted Lists/src/Solution.java
+++ 21. Merge Two Sorted Lists/src/Solution.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
 
-public class Solution {
+
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+
+        while (l1 != null && l2 !=null) {
+            if (l1.val > l2.val) {
+                cur.next = l2;
+                l2 = l2.next;
+            } else {
+                cur.next = l1;
+                l1 = l1.next;
+            }
+            cur = cur.next;
+        }
+
+        if (l1 != null) {
+            cur.next = l1;
+        }
+
+        if (l2 != null) {
+            cur.next = l2;
+        }
+
+        return dummy.next;
+    }
 }
