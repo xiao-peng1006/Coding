e9cb2c8b863be3e4d3aa5f4fe339c5a74fb1f25e
Xiao Peng <xiao.peng1006@gmail.com>
2021-11-15 00:48:23 -0800

    M, recursive

---
+++ 138. Copy List with Random Pointer/src/Solution.java
@@ -0,0 +1,38 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+
+    HashMap<Node, Node> visitedHash = new HashMap<Node, Node>();
+
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+
+        if (this.visitedHash.containsKey(head)) {
+            return this.visitedHash.get(head);
+        }
+
+        Node node = new Node(head.val, null, null);
+
+        this.visitedHash.put(head, node);
+
+        node.next = this.copyRandomList(head.next);
+        node.random = this.copyRandomList(head.random);
+
+        return node;
+    }
+}
