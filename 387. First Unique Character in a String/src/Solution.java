bf815e901d67ee71d596ac94c0fd4f2d74737c50
Xiao Peng <xiao.peng1006@gmail.com>
2021-11-16 01:28:59 -0800

    count all the characters, then loop the string again

---
+++ 387. First Unique Character in a String/src/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c) && map.get(c) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
