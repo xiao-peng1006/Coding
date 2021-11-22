0cf5622287058f021065fe4e50bd6597e7083fbc
Xiao Peng <xiao.peng1006@gmail.com>
2021-11-22 00:15:43 -0800

    one pass, find the min price point and calculate the profit in the meantime

---
+++ 121. Best Time to Buy and Sell Stock/src/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0, minPrice = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else {
+                if (prices[i] - minPrice > maxProfit) {
+                    maxProfit = prices[i] - minPrice;
+                }
+            }
+        }
+        return maxProfit;
+    }
+}
