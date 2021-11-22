79c186eaa4a93c22dac0b8b116e6a3e8e87325b9
Xiao Peng <xiao.peng1006@gmail.com>
2021-11-21 23:54:45 -0800

    Dynamic programming approach

--- 139. Word Break/src/Solution.java
+++ 139. Word Break/src/Solution.java
@@ -1,22 +1,15 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        return helper(s, wordDict, 0, new Boolean[s.length()]);
-    }
-
-    public boolean helper(String s, List<String> wordDict, int start, Boolean[] memo) {
-        if (start == s.length()) {
-            return true;
-        }
-
-        if (memo[start] != null) {
-            return memo[start];
-        }
-
-        for (int end = start + 1; end <= s.length(); end++) {
-            if (wordDict.contains(s.substring(start, end)) && helper(s, wordDict, end, memo)) {
-                return memo[start] = true;
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordDict.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
             }
         }
-        return memo[start] = false;
+        return dp[s.length()];
     }
 }
