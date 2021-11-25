11bab81bcda79f251d5b93bb3b0d619aa99dd0f7
Xiao Peng <xiao.peng1006@gmail.com>
2021-11-24 23:17:18 -0800

    dynamic programming

--- 509. Fibonacci Number/src/Solution.java
+++ 509. Fibonacci Number/src/Solution.java
@@ -1,8 +1,14 @@
 class Solution {
     public int fib(int n) {
-        if (n <= 1) {
-            return n;
+        if (n == 0) {
+            return 0;
         }
-        return fib(n - 1) + fib(n - 2);
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0; dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 }
