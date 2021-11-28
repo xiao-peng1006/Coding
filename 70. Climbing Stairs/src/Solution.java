af2f63a4b0bf1c19e492ea0adeebb89146c4ee86
Xiao Peng <xiao.peng1006@gmail.com>
2021-11-28 00:04:00 -0800

    dynamic programming

--- 70. Climbing Stairs/src/Solution.java
+++ 70. Climbing Stairs/src/Solution.java
@@ -1,23 +1,15 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] memo = new int[n + 1];
-        return helper(0, n, memo);
-    }
-
-    public int helper(int i, int n, int[] memo) {
-        if (i > n) {
-            return 0;
-        }
-
-        if (i == n) {
+        if (n == 1) {
             return 1;
         }
 
-        if (memo[i] > 0) {
-            return memo[i];
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-
-        memo[i] = helper(i + 1, n, memo) + helper(i + 2, n, memo);
-        return memo[i];
+        return dp[n];
     }
 }
