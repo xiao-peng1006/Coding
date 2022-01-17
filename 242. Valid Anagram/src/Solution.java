f225288f54cbd8cbf24627d364d8c7f287f3b210
Xiao Peng <xiao.peng1006@gmail.com>
2022-01-17 00:29:23 -0800

    use array int[26] to count the characters

---
+++ 242. Valid Anagram/src/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] counts = new int[26];
+        for (char c : s.toCharArray()) {
+            counts[c - 'a']++;
+        }
+
+        for (char c : t.toCharArray()) {
+            counts[c - 'a']--;
+        }
+
+        for (int count : counts) {
+            if (count != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
