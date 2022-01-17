7c2b2571dc8ebd68309062e3dc291765b5b3af6f
Xiao Peng <xiao.peng1006@gmail.com>
2022-01-17 00:38:59 -0800

    use array to count the characters

---
+++ 383. Ransom Note/src/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()) {
+            return false;
+        }
+
+        int[] count = new int[26];
+        for (char c : magazine.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        for (char c : ransomNote.toCharArray()) {
+            count[c - 'a']--;
+            if (count[c - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
