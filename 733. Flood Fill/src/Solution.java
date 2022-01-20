4c2c8300fcc2a36800c8e9be006e98271b3bea05
Xiao Peng <xiao.peng1006@gmail.com>
2022-01-19 18:05:39 -0800

    dfs

--- 733. Flood Fill/src/Solution.java
+++ 733. Flood Fill/src/Solution.java
@@ -1,46 +1,21 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
-
-        boolean[][] visited = new boolean[image.length][image[0].length];
-        Queue<Pair<Integer, Integer>> q = new LinkedList<>();
-        q.add(new Pair(sr, sc));
-        visited[sr][sc] = true;
-        int target = image[sr][sc];
-
-        while (!q.isEmpty()) {
-            int count = q.size();
-            for (int i = 0; i < count; i++) {
-                Pair<Integer, Integer> idx = q.poll();
-                int r = idx.getKey();
-                int c = idx.getValue();
-
-                if (image[r][c] == target) {
-                    image[r][c] = newColor;
-                } else {
-                    continue;
-                }
-
-                if (r - 1 >= 0 && !visited[r-1][c]) {
-                    q.add(new Pair(r - 1, c));
-                    visited[r - 1][c] = true;
-                }
-
-                if ((r + 1 < image.length && !visited[r+1][c])) {
-                    q.add(new Pair(r + 1, c));
-                    visited[r + 1][c] = true;
-                }
-
-                if (c - 1 >= 0 && !visited[r][c-1]) {
-                    q.add(new Pair(r, c - 1));
-                    visited[r][c - 1] = true;
-                }
-
-                if ((c + 1 < image[0].length && !visited[r][c+1])) {
-                    q.add(new Pair(r, c + 1));
-                    visited[r][c + 1] = true;
-                }
-            }
+        int color = image[sr][sc];
+        if (color != newColor) {
+            dfs(image, sr, sc, color, newColor);
         }
         return image;
+    }
+
+    void dfs(int[][] image, int r, int c, int color, int newColor) {
+        if (image[r][c] != color) {
+            return;
+        }
+
+        image[r][c] = newColor;
+        if (r > 0) dfs(image, r-1, c, color, newColor);
+        if (c > 0) dfs(image, r, c-1, color, newColor);
+        if (r+1 < image.length) dfs(image, r+1, c, color, newColor);
+        if (c+1 < image[0].length) dfs(image, r, c+1, color, newColor);
     }
 }
