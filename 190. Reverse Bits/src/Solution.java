d777cb85635713fb7ba5e7a1308819cb6a9a8240
Xiao Peng <xiao.peng1006@gmail.com>
2022-01-23 23:11:15 -0800

    get last bit: (n>>i)&1, add to ans:(ans<<1)|bit

---
+++ 190. Reverse Bits/src/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int ans = 0;
+        for (int i = 0; i < 32; i++) {
+            int bit = ((n >> i) & 1);
+            ans = (ans << 1) | bit;
+        }
+        return ans;
+    }
+}
