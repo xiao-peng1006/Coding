3a12b627ac18b1d7d2f3fd7092b8a645187ca502
Xiao Peng <xiao.peng1006@gmail.com>
2022-01-25 23:02:32 -0800

    two-pointer, be careful on duplicate numbers

--- 15. 3Sum/src/Solution.java
+++ 15. 3Sum/src/Solution.java
@@ -4,23 +4,23 @@ class Solution {
         if (nums.length < 3) return ans;
 
         Arrays.sort(nums);
-        int left, right, target;
-        List<Integer> list;
-        for (int i = 0; i < nums.length - 2; i++) {
-            left = i + 1;
-            right = nums.length - 1;
-            target = -nums[i];
-            while (left < right) {
-                if (nums[left] + nums[right] < target) {
-                    left++;
-                } else if (nums[left] + nums[right] > target) {
-                    right--;
-                } else {
-                    list = Arrays.asList(nums[i], nums[left], nums[right]);
-                    if (!ans.contains(list)) {
-                        ans.add(list);
+
+        for (int i = 0; i < nums.length && nums[i] <= 0; i++) {
+            if (i == 0 || nums[i - 1] != nums[i]) {
+                int low = i + 1;
+                int high = nums.length - 1;
+                while (low < high) {
+                    int sum = nums[low] + nums[high] + nums[i];
+                    if (sum < 0) {
+                        low++;
+                    } else if (sum > 0) {
+                        high--;
+                    } else {
+                        ans.add(Arrays.asList(nums[i], nums[low++], nums[high--]));
+                        while (low < high && nums[low] == nums[low - 1]) {
+                            low++;
+                        }
                     }
-                    left++;
                 }
             }
         }
