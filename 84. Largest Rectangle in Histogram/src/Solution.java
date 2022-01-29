5c82c9f97d1156779bc81cd1844edad00cf1a128
Xiao Peng <xiao.peng1006@gmail.com>
2022-01-29 10:54:39 -0800

    brutal force, LTE, optimized from O(n^3) to O(n^2)

--- 84. Largest Rectangle in Histogram/src/Solution.java
+++ 84. Largest Rectangle in Histogram/src/Solution.java
@@ -2,11 +2,9 @@ class Solution {
     public int largestRectangleArea(int[] heights) {
         int maxArea = 0;
         for (int i = 0; i < heights.length; i++) {
+            int minHeight = Integer.MAX_VALUE;
             for (int j = i; j < heights.length; j++) {
-                int minHeight = Integer.MAX_VALUE;
-                for (int k = i; k <= j; k++) {
-                    minHeight = Math.min(minHeight, heights[k]);
-                }
+                minHeight = Math.min(minHeight, heights[j]);
                 maxArea = Math.max(maxArea, minHeight * (j - i +1));
             }
         }
