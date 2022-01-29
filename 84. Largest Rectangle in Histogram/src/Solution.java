618ae02cdbb39032c9425c558535be66d6de5239
Xiao Peng <xiao.peng1006@gmail.com>
2022-01-29 10:52:35 -0800

    brutal force, LTE, double loop

---
+++ 84. Largest Rectangle in Histogram/src/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int maxArea = 0;
+        for (int i = 0; i < heights.length; i++) {
+            for (int j = i; j < heights.length; j++) {
+                int minHeight = Integer.MAX_VALUE;
+                for (int k = i; k <= j; k++) {
+                    minHeight = Math.min(minHeight, heights[k]);
+                }
+                maxArea = Math.max(maxArea, minHeight * (j - i +1));
+            }
+        }
+        return maxArea;
+    }
+}
