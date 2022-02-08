b3f3f4f6082d59ed4502e19eef485c5f31ce5c6d
Xiao Peng <xiao.peng1006@gmail.com>
2022-02-08 00:24:06 -0800

    recursion

---
+++ 258. Add Digits/src/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int addDigits(int num) {
+        if (num < 10) {
+            return num;
+        }
+
+        int newNum = 0;
+        while (num != 0) {
+            newNum += num%10;
+            num /= 10;
+        }
+
+        return addDigits(newNum);
+    }
+}
